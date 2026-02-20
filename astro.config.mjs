8d08aefe193d3191defb33a6d1c84876c40205f3
Carolina <carolinasemprini@gmail.com>
2026-02-20 18:49:11 -0300

    Fix remote image domain for WP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
-    vite:{
-            plugins: [tailwindcss()]
-      
-    },
-    image:{
-        domains:['coffeshop.local'],
-    }
+  // ✅ Recomendado para deploy (podés cambiarlo luego)
+  // Si todavía no tenés el subdominio, dejalo comentado.
+  // site: 'https://cafe.semprinicarolina.com.ar',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  image: {
+    domains: ['coffeshop.local', 'cafe.semprinicarolina.com.ar'],
+  },
+
+  adapter: netlify(),
 });
